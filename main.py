fd7507da946e3c62420a342768eef9388a58af77
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 12:29:58 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Katta matn fayldan eng kop uchrashadigan 5 ta sozni toping.")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
